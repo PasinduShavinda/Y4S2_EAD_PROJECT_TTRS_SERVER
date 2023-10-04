f3bac70e81ff7d311cc96485d60977ae67cb4754
pasindushv <it20140298@my.sliit.lk>
2023-10-04 22:21:00 +0530

    IT20140298 - Folder Structure Changes & DB Connection Established

--- Data/DBSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace TavelEase_WebService.Data
-{
-    public class DatabaseSettings
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-        public string UserCollectionName { get; set; } = string.Empty;
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
-﻿using TavelEase_WebService.Data;
+﻿using System.Text;
+using AspNetCore.Identity.MongoDbCore.Extensions;
+using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+// Add services to the container.
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeSerializer(MongoDB.Bson.BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
+
+
+//add mongoIdentityConfiguration...
+var mongoDbIdentityConfig = new MongoDbIdentityConfiguration
+{
+    MongoDbSettings = new MongoDbSettings
+    {
+        ConnectionString = "mongodb://localhost:27017",
+        DatabaseName = "eadprojectdb"
+    },
+    IdentityOptionsAction = options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 8;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireLowercase = false;
+
+        //lockout
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+
+        options.User.RequireUniqueEmail = true;
+
+    }
+
+};
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
-
