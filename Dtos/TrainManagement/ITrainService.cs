98da3231a838b3019405273d2f6c8bbcba38282b
Kalansooriya S. H <sugandhi.kalansooriya@gmail.com>
2023-10-05 23:09:06 +0530

    Train management API implemented

---
+++ Dtos/TrainManagement/ITrainService.cs
@@ -0,0 +1,20 @@
+﻿//namespace TravelEase_WebService.Dtos.TrainManagement
+
+// Services/ITrainService.cs
+// Services/ITrainService.cs
+using System;
+using System.Collections.Generic;
+using TravelEase_WebService.TrainModels;
+
+namespace TravelEase_WebService.Services
+{
+    public interface ITrainService
+    {
+        IEnumerable<Train> GetTrains();
+        Train GetTrainById(Guid id);
+        void AddTrain(Train train);
+        void UpdateTrain(Train train);
+        void DeleteTrain(Guid id);
+    }
+}
+
---
+++ Models/TrainManagement/TrainDetails.cs
@@ -0,0 +1,15 @@
+﻿// Models/TrainsDetails.cs
+// Models/Train.cs
+using System;
+
+namespace TravelEase_WebService.TrainModels
+{
+    public class Train
+    {
+        public Guid Id { get; set; }
+        public string TrainName { get; set; }
+        public string DepartureCity { get; set; }
+        public string ArrivalCity { get; set; }
+        // Add other properties as needed
+    }
+}
